154fd4985a8e1064c803acb7a7534ba4e0e3e2a2
Ahmad Alkurdi <ahmad.alkurdi@tarjama.com>
2025-02-18 11:01:44 +0300

    changes

--- length_service.py
+++ length_service.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, jsonify
+import os
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry import trace
 
+# Initialize OpenTelemetry Tracing
+tracer_provider = TracerProvider()
+trace.set_tracer_provider(tracer_provider)
+
+# Use OTLP Exporter to send traces to Jaeger
+otlp_endpoint = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://jaeger-collector.jaeger:4317")
+otlp_exporter = OTLPSpanExporter(endpoint=otlp_endpoint, insecure=True)
+span_processor = BatchSpanProcessor(otlp_exporter)
+tracer_provider.add_span_processor(span_processor)
+
+# Initialize Flask App
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
 
 @app.route('/length', methods=['POST'])
 def string_length():
     input_data = request.get_data(as_text=True).strip()
     if not input_data:
         return "No input provided", 400
-    
+
     return str(len(input_data))
 
 @app.route('/health', methods=['GET'])
